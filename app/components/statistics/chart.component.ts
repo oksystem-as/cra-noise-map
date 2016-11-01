98e8efeaebc2108684cb848ed9e8c97479545049
Petr Diblík <diblik@oksystem.cz>
2016-11-01 15:32:16 +0100

    zobrazeni grafu

--- app/components/statistics/chart.component.ts
+++ app/components/statistics/chart.component.ts
@@ -87,7 +87,7 @@ export class ChartComponent implements AfterViewInit {
             display: false,
         },
         // maintainAspectRatio: true,
-        responsive: true,
+        responsive: false,
         onClick: this.pointClick.bind(this, this.sensorsSharedService),
     }
 
