598a81534b1aafaa53dbb1d95b95d02b919606a2
Petr Diblík <diblik@oksystem.cz>
2016-11-04 11:06:30 +0100

    logo pro landscape se rozsiri

--- app/components/map/logo.component.ts
+++ app/components/map/logo.component.ts
@@ -1,6 +1,7 @@
-import { ChangeDetectionStrategy, Component, Input, ViewEncapsulation } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input, ViewEncapsulation, ChangeDetectorRef } from '@angular/core';
 import { Logger } from "angular2-logger/core";
 import { SensorsSharedService, Overlay, OverlayGroup, Events } from '../sensors-shared.service';
+import { ResponsiveState } from 'ng2-responsive';
 
 @Component({
   selector: 'logo',
@@ -9,4 +10,25 @@ import { SensorsSharedService, Overlay, OverlayGroup, Events } from '../sensors-
 })
 export class LogoComponent {
 
+  // isLandscapeInternal: boolean;
+ 
+  // constructor(private changeDetectorRef: ChangeDetectorRef, responsiveState: ResponsiveState) {
+  //   responsiveState.orientationObserver.subscribe(orientation => {
+  //     // orientation:['landscape','portrait'],
+  //      console.log("orientationObserver", orientation);
+  //     this.isLandscapeInternal = orientation === "landscape";
+  //     changeDetectorRef.detectChanges();
+  //   })
+  // }
+
+  // isLandscape():boolean {
+  //   if(this.isLandscapeInternal === undefined){
+  //     console.log("isLandscapeInternal :( )", this.isLandscapeInternal);
+  //   }
+  //   console.log("isLandscapeInternal", this.isLandscapeInternal);
+  //   return this.isLandscapeInternal;
+  // }
 }
+
+
+
