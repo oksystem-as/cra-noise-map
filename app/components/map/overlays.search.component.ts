1c61acd52c965ef1121ed6de0ee097df3ee01862
Pavel Cihlář <cihlar@oksystem.cz>
2016-11-10 09:58:42 +0100

    pridana lupa a zmenena velikost pisma v desktopu

--- app/components/map/overlays.search.component.ts
+++ app/components/map/overlays.search.component.ts
@@ -1,5 +1,7 @@
 import { ChangeDetectionStrategy, Component, Input, ViewEncapsulation } from '@angular/core';
 import { Logger } from "angular2-logger/core";
+import { MapComponent } from './../map.component';
+import { SensorsSharedService, Overlay, OverlayGroup, Events } from '../sensors-shared.service';
 
 @Component({
   selector: 'overlays-search',
@@ -8,5 +10,21 @@ import { Logger } from "angular2-logger/core";
 })
 
 export class OverlaysSearchComponent {
- 
+
+  private searchBox: google.maps.places.SearchBox;
+
+  constructor(private log: Logger, private sensorsSharedService: SensorsSharedService) {
+    sensorsSharedService.listenEventData(Events.mapInstance).subscribe(map => {
+      if (map != undefined) {
+        var input = document.getElementById('pac-input') as HTMLInputElement;
+        var searchBox = new google.maps.places.SearchBox(input);
+      }
+    });
+  }
+
+  private triggerSearch() {
+    google.maps.event.trigger(this.searchBox, 'place_changed');
+    google.maps.event.trigger(this.searchBox, 'bounds_changed');
+  }
+
 }
