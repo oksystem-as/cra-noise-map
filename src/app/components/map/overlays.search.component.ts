7b2e469fc79b80a2950b1016134dcfaadd1eb2e9
Pavel Cihlář <cihlar@oksystem.cz>
2016-11-11 17:50:39 +0100

    uprava velikosti divu u serchboxu

--- src/app/components/map/overlays.search.component.ts
+++ src/app/components/map/overlays.search.component.ts
@@ -12,14 +12,33 @@ export class OverlaysSearchComponent {
 
   private xl: string = "xl";
   private lg: string = "lg";
+  private md: string = "md";
+  private sm: string = "sm";
+  private xs: string = "xs";
+
   private firefox: string = "firefox";
 
-  private isBigInternal: boolean;
-  private isFirefoxInternal: boolean;
+  private isExtraLargeInternal: boolean = false;
+  private isLargeInternal: boolean = false;
+  private isMediumInternal: boolean = false;
+  private isSmallInternal: boolean = false;
+  private isExtraSmallInternal: boolean = false;
+
+  private isFirefoxInternal: boolean = false;
 
   constructor(private log: Logger, responsiveState: ResponsiveState) {
     responsiveState.elementoObservar.subscribe(width => {
-      this.isBigInternal = (width === this.xl || width === this.lg);
+      this.isExtraLargeInternal = false;
+      this.isLargeInternal = false;
+      this.isMediumInternal = false;
+      this.isSmallInternal = false;
+      this.isExtraSmallInternal = false;
+
+      this.isExtraLargeInternal = (width === this.xl);
+      this.isLargeInternal = ( width === this.lg);
+      this.isMediumInternal = ( width === this.md);
+      this.isSmallInternal = ( width === this.sm);
+      this.isExtraSmallInternal = ( width === this.xs);
     });
 
     responsiveState.browserObserver.subscribe(browser => {
@@ -30,11 +49,39 @@ export class OverlaysSearchComponent {
     });
   }
 
-  private isBig(): boolean {
-    if (this.isBigInternal == undefined) {
-      throw "isBigInternal neni definovan";
+  private isExtraLarge(): boolean {
+    if (this.isExtraLargeInternal == undefined) {
+      throw "isExtraLargeInternal neni definovan";
     }
-    return this.isBigInternal;
+    return this.isExtraLargeInternal;
+  }
+
+  private isLarge(): boolean {
+    if (this.isLargeInternal == undefined) {
+      throw "isLargeInternal neni definovan";
+    }
+    return this.isLargeInternal;
+  }
+
+  private isMedium(): boolean {
+    if (this.isMediumInternal == undefined) {
+      throw "isMediumInternal neni definovan";
+    }
+    return this.isMediumInternal;
+  }
+
+  private isSmall(): boolean {
+    if (this.isSmallInternal == undefined) {
+      throw "isSmallInternal neni definovan";
+    }
+    return this.isSmallInternal;
+  }
+
+  private isExtraSmall(): boolean {
+    if (this.isExtraSmallInternal == undefined) {
+      throw "isExtraSmallInternal neni definovan";
+    }
+    return this.isExtraSmallInternal;
   }
 
   private isFirefox(): boolean {
