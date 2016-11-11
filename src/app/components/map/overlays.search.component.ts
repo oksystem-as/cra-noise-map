92a0b2a54bf692c077dc14e69a2c43ea0657bc58
Pavel Cihlář <cihlar@oksystem.cz>
2016-11-11 15:10:33 +0100

    zmena velikosti search boxu u firefoxu

--- src/app/components/map/overlays.search.component.ts
+++ src/app/components/map/overlays.search.component.ts
@@ -2,7 +2,7 @@ import { ChangeDetectionStrategy, Component, Input, ViewEncapsulation } from '@a
 import { Logger } from "angular2-logger/core";
 import { ResponsiveState, ResponsiveConfig } from 'ng2-responsive';
 
-@Component({   
+@Component({
   selector: 'overlays-search',
   templateUrl: './overlays.search.component.html',
   styleUrls: ['./overlays.search.component.css'],
@@ -12,26 +12,37 @@ export class OverlaysSearchComponent {
 
   private xl: string = "xl";
   private lg: string = "lg";
+  private firefox: string = "firefox";
+
   private isBigInternal: boolean;
+  private isFirefoxInternal: boolean;
 
   constructor(private log: Logger, responsiveState: ResponsiveState) {
     responsiveState.elementoObservar.subscribe(width => {
-      this.isBigInternal = false;
-      if (width === this.xl) {
-         this.isBigInternal = true;
+      this.isBigInternal = (width === this.xl || width === this.lg);
+    });
+
+    responsiveState.browserObserver.subscribe(browser => {
+      this.isFirefoxInternal = false;
+      if (browser === this.firefox) {
+        this.isFirefoxInternal = true;
       }
-      if (width === this.lg) {
-         this.isBigInternal = true;
-      }
-    })
+    });
   }
 
-  private isBig():boolean {
+  private isBig(): boolean {
     if (this.isBigInternal == undefined) {
       throw "isBigInternal neni definovan";
     }
     return this.isBigInternal;
-   }
+  }
+
+  private isFirefox(): boolean {
+    if (this.isFirefoxInternal == undefined) {
+      throw "isFirefoxInternal neni definovan";
+    }
+    return this.isFirefoxInternal;
+  }
 }
 
 // Následující observer vrací šířku diplaye v pixelech
